d411e9243c8cacc6e7ace28eac02334cf626e692
Ian Thomson <ianbthomson@gmail.com>
2012-01-24 14:17:58 -0500

    added custom scopes to server model

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,4 +2,9 @@ class Server < ActiveRecord::Base
 
   validates_uniqueness_of :name
 
+  default_scope :order => "name ASC"
+  scope :all_prod, :conditions => { :test_server => 0 }
+  scope :all_test, :conditions => { :test_server => 1 }
+  scope :all_vm, :conditions => { :vm_server => 1 } 
+
 end
