d6c329cb49a756997a2f77679af9362ca1515a37
Ian Thomson <ianbthomson@gmail.com>
2012-01-30 09:50:46 -0500

    fixed signon to allow username

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password
+  attr_accessible :username, :password
 
 
 end
