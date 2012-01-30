a937ffa442bdc27f52c4aa0a651c5894a327d24a
Ian Thomson <ianbthomson@gmail.com>
2012-01-30 15:53:50 -0500

    added regex validation for IP addresses

--- app/models/server.rb
+++ app/models/server.rb
@@ -2,6 +2,9 @@ class Server < ActiveRecord::Base
 
   validates_uniqueness_of :name
 
+  validates :ip, :format => {:with => /\A(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]?)\z/, :message => "Invalid IP Address format. [0-255].[0-255].[0-255].[0-255]" } 
+
+
   default_scope :order => "name ASC"
   scope :all_prod, :conditions => { :test_server => 0 }
   scope :all_test, :conditions => { :test_server => 1 }
